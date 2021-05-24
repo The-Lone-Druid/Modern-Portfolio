63d3bb007b7eed769b0adc3ccbef5def635797ff
The-Lone-Druid <zahid.786shaikh@gmail.com>
2021-05-24 17:03:54 +0530

    Speed optimization

--- dist/js/app.js
+++ dist/js/app.js
@@ -25,72 +25,6 @@ overlay.addEventListener('click', function() {
     toggleSideBar(false);
 })
 
-// Display projects
-easyhttp = new EasyHTTP();
-
-const projectContainer = document.querySelector('.projectContainer');
-easyhttp.get('dist/js/content/projects.json')
-    .then(projects => {
-        projects.forEach(project => {
-            projectContainer.innerHTML += `
-            <div class="col-md-6 d-flex justify-content-end mt-4">
-                <div class="project-card">
-                    <div class="project-image">
-                        <img src="${project.projectImage}" alt="" class="w-100 h-100">
-                    </div>
-                    <div class="project-text">
-                        <h3>${project.projectName}</h3>
-                        <p>
-                            ${project.projectInfo}
-                        </p>
-                        <a href="${project.projectLink}" target="_blank" class="nav-link"><i class="fas fa-external-link"></i> Visit Website</a>
-                    </div>
-                </div>
-            </div>
-            `;
-        })
-    })
-    .catch(err => console.log(err));
-
-const skillContainer = document.querySelector('.skillContainer');
-easyhttp.get('dist/js/content/skills.json')
-    .then(skills => {
-        skills.forEach(skill => {
-            skillContainer.innerHTML += `
-            <div class="col-md-4 col-lg-3 col-sm-12 mt-5 d-flex align-items-center justify-content-center">
-                <div class="skill-card">
-                    <div class="skill-icon">
-                        <i class="${skill.skillIcon} gradient-text"></i>
-                    </div>
-                    <h4 class="skill-name">${skill.skillName}</h4>
-                </div>
-            </div>
-            `;
-        });
-    })
-    .catch(err => console.log(err));
-
-const servicesContainer = document.querySelector('.servicesContainer');
-easyhttp.get('dist/js/content/services.json')
-    .then(services => {
-        services.forEach(service => {
-            servicesContainer.innerHTML += `
-            <div class="col-lg-6 col-xl-4 mt-5 d-flex justify-content-center">
-                <div class="service-card">
-                    <div class="service-icon d-flex flex-column align-items-center">
-                        <i class="${service.serviceIcon} fa-4x gradient-text"></i>
-                        <h5 class="gradient-text mt-4 fw-bold">${service.serviceHeading}</h5>
-                    </div>
-                    <div class="service-text px-2">
-                        ${service.serviceText}
-                    </div>
-                </div>
-            </div>            
-            `;
-        });
-    })
-    .catch(err => console.log(err));
-
 // form clear fields
 inputFields = document.querySelectorAll('input');
 clearButton = document.querySelector('#clearFields');
@@ -113,3 +47,72 @@ function toggleSideBar(status) {
         overlay.classList.remove('overlay-toggle');
     }
 }
+
+// load content
+document.addEventListener('DOMContentLoaded', () => {
+    // Display projects
+    easyhttp = new EasyHTTP();
+
+    const projectContainer = document.querySelector('.projectContainer');
+    easyhttp.get('dist/js/content/projects.json')
+        .then(projects => {
+            projects.forEach(project => {
+                projectContainer.innerHTML += `
+                <div class="col-md-6 d-flex justify-content-end mt-4">
+                    <div class="project-card">
+                        <div class="project-image d-flex align-items-center">
+                            <img src="${project.projectImage}" alt="" class="w-100">
+                        </div>
+                        <div class="project-text">
+                            <h3>${project.projectName}</h3>
+                            <p>
+                                ${project.projectInfo}
+                            </p>
+                            <a href="${project.projectLink}" target="_blank" class="nav-link"><i class="fas fa-external-link"></i> Visit Website</a>
+                        </div>
+                    </div>
+                </div>
+                `;
+            })
+        })
+        .catch(err => console.log(err));
+
+    const skillContainer = document.querySelector('.skillContainer');
+    easyhttp.get('dist/js/content/skills.json')
+        .then(skills => {
+            skills.forEach(skill => {
+                skillContainer.innerHTML += `
+                <div class="col-md-4 col-lg-3 col-sm-12 mt-5 d-flex align-items-center justify-content-center">
+                    <div class="skill-card">
+                        <div class="skill-icon">
+                            <i class="${skill.skillIcon} gradient-text"></i>
+                        </div>
+                        <h4 class="skill-name">${skill.skillName}</h4>
+                    </div>
+                </div>
+                `;
+            });
+        })
+        .catch(err => console.log(err));
+
+    const servicesContainer = document.querySelector('.servicesContainer');
+    easyhttp.get('dist/js/content/services.json')
+        .then(services => {
+            services.forEach(service => {
+                servicesContainer.innerHTML += `
+                <div class="col-lg-6 col-xl-4 mt-5 d-flex justify-content-center">
+                    <div class="service-card">
+                        <div class="service-icon d-flex flex-column align-items-center">
+                            <i class="${service.serviceIcon} fa-4x gradient-text"></i>
+                            <h5 class="gradient-text mt-4 fw-bold">${service.serviceHeading}</h5>
+                        </div>
+                        <div class="service-text px-2">
+                            ${service.serviceText}
+                        </div>
+                    </div>
+                </div>            
+                `;
+            });
+        })
+        .catch(err => console.log(err));
+})
